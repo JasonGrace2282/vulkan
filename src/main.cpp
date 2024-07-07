9723d5034667cba4064e5a3268835c1ac455fc36
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-07-07 09:36:29 -0400

    Set up VkInstance, and extensions

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,127 @@
+#ifdef LSP_COMPLETION
+#include <vulkan/vulkan_core.h>
+#endif
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <string.h>
+#include <vector>
 
-int main() {
-    glfwInit();
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
+class Application {
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  GLFWwindow *window;
+  VkInstance instance;
+
+  void initWindow() {
+    if (!glfwInit()) {
+      throw std::runtime_error("Failed to initialize GLFW");
+    }
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    if (!window) {
+      throw std::runtime_error("Failed to create window");
+    }
+  }
 
+  void initVulkan() { createInstance(); }
+
+  void createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    // applies to the entire program in Vulkan
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    // global extensions setup
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
 
-    std::cout << extensionCount << " extensions supported\n";
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
+    // make sure that all the glfw extensions are present
+    // I think (?) that glfwExtensions/glfwExtensionCount are copied so there
+    // shouldn't be any UB
+    checkExtensionsPresent(extensions, glfwExtensions, glfwExtensionCount);
 
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    // which global validation layers to enable
+    createInfo.enabledLayerCount = 0;
+
+    // finally create the actual instance
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance!");
     }
+  }
+
+  /// Check if required is present in extensions
+  void checkExtensionsPresent(std::vector<VkExtensionProperties> extensions, const char **required,
+                              uint32_t required_count) {
+    for (uint32_t i = 0; i < required_count; i++) {
+      auto ext = required[i];
+      bool found = false;
+      for (auto property : extensions) {
+        if (strcmp(ext, property.extensionName) == 0) {
+          found = true;
+        }
+      }
+      if (!found) {
+        throw std::runtime_error("Extension " + std::string(ext) + " not supported!");
+      }
+    }
+  }
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    // clean up instance, without any callback
+    vkDestroyInstance(instance, nullptr);
 
     glfwDestroyWindow(window);
-
     glfwTerminate();
+  }
+};
 
-    return 0;
+int main() {
+  Application app;
+
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
