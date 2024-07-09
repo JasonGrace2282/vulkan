596aff867eec509b47369346380c62c17c458c68
JasonGrace2282 <aarush.deshpande@gmail.com>
2024-07-08 23:01:45 -0400

    finally finish the render pass

--- src/main.cpp
+++ src/main.cpp
@@ -148,6 +148,7 @@ private:
   std::vector<VkImageView> swapChainImageViews;
   VkPipelineLayout pipelineLayout;
   VkRenderPass renderPass;
+  VkPipeline graphicsPipeline;
 
   void initWindow() {
     if (!glfwInit()) {
@@ -705,6 +706,29 @@ private:
       throw std::runtime_error("failed to create pipeline layout!");
     }
 
+    // create the pipeline (finally!)
+    VkGraphicsPipelineCreateInfo pipelineInfo{};
+    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipelineInfo.stageCount = 2;
+    pipelineInfo.pStages = shaderStages;
+    pipelineInfo.pVertexInputState = &vertexInputInfo;
+    pipelineInfo.pInputAssemblyState = &inputAssembly;
+    pipelineInfo.pViewportState = &viewportState;
+    pipelineInfo.pRasterizationState = &rasterizer;
+    pipelineInfo.pMultisampleState = &multisampling;
+    pipelineInfo.pDepthStencilState = nullptr; // Optional
+    pipelineInfo.pColorBlendState = &colorBlending;
+    pipelineInfo.pDynamicState = &dynamicState;
+    pipelineInfo.layout = pipelineLayout;
+    pipelineInfo.renderPass = renderPass; // pass handle
+    pipelineInfo.subpass = 0;
+    pipelineInfo.basePipelineHandle = VK_NULL_HANDLE; // Optional
+    pipelineInfo.basePipelineIndex = -1;              // Optional
+
+    if (vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create graphics pipeline!");
+    }
+
     // all compilation happens at pipeline creation, so we can destroy
     // the shaders after
     vkDestroyShaderModule(device, fragShaderModule, nullptr);
@@ -736,6 +760,7 @@ private:
       DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
     }
 
+    vkDestroyPipeline(device, graphicsPipeline, nullptr);
     vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
     vkDestroyRenderPass(device, renderPass, nullptr);
 
